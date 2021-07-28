74fe95741393f1ccf017aeb08a1e04d726076616
Wllie <taywilliam197@hotmail.com>
2021-07-28 13:34:42 -0500

    Commit inicial

---
+++ ejemplo3.java
@@ -0,0 +1,11 @@
+/**
+ * ejemplo3
+ */
+public class ejemplo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Hola2");
+        System.out.println("Hola3");
+    }
+}
